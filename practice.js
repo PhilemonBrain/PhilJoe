3d62433c07db4f15caef94db774c9ce28c265219
Philbrainy <philbrainy@gmail.com>
2020-07-11 17:19:22 -0700

    I Left a bug in the code

--- practice.js
+++ practice.js
@@ -5,5 +5,16 @@ function greatest(arr){
     }
     return max;
 }
-console.log(greatest([5, 66, 89, 90, 4])) // should return 90
+// console.log(greatest([5, 66, 89, 90, 4])) // should return 90
 
+function compareGreatest(arr, arr2, arr3){
+    a = greatest(arr)
+    b = greatest(arr2)
+    c = greatest(arr3)
+    return(greatest({a,b,c}))
+}
+
+mylist = [43,67,32,76,23,54]
+mylist2 = [89,92,12,77,20,46]
+mylist3 = [79,123,63,44,95,17]
+console.log(compareGreatest(mylist, mylist2, mylist3))

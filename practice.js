f3c7cd75e539c0962a6c7e3a88d6a837cd3b8f62
Joefamouze <47423105+Joefamouze@users.noreply.github.com>
2020-07-13 09:58:50 +0100

    Update practice.js
    Arrays are not iterable. So i cleared all those lines of code. all we needed was one.

--- practice.js
+++ practice.js
@@ -1,20 +1,11 @@
-function greatest(arr){
-    let max = arr[0];
+function compareGreatest(a, b, c){
+    function greatest(arr){
+    var max = arr[0]
     for (const item of arr){
-        if (max < item) max = item;
+        if (item > max) max = item;
     }
     return max;
 }
-// console.log(greatest([5, 66, 89, 90, 4])) // should return 90
-
-function compareGreatest(arr, arr2, arr3){
-    a = greatest(arr)
-    b = greatest(arr2)
-    c = greatest(arr3)
-    return(greatest({a,b,c}))
+   
+    console.log(greatest(a), greatest(b), greatest(c))
 }
-
-mylist = [43,67,32,76,23,54]
-mylist2 = [89,92,12,77,20,46]
-mylist3 = [79,123,63,44,95,17]
-console.log(compareGreatest(mylist, mylist2, mylist3))
